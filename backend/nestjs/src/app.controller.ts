23104d3eb4f6bd0192c528d5a3b1046748b80d62
wonki LEE <dnjs1490@naver.com>
2025-01-26 23:10:05 +0900

    개발환경 표준화  작업

--- backend/nestjs/src/app.controller.ts
+++ backend/nestjs/src/app.controller.ts
@@ -1,6 +1,7 @@
 import { Controller, Get } from '@nestjs/common';
 import { AppService } from './app.service';
 
+
 @Controller('api')
 export class AppController {
   constructor(private readonly appService: AppService) {}
@@ -9,6 +10,7 @@ export class AppController {
   getData() {
     return { message: '바다이야기에 오신 것을 환영합니다' };
   }
+  @Get()
   getHello(): string {
     return this.appService.getHello();
   }
--- frontend/vuejs/src/components/ExampleComponent.vue
+++ frontend/vuejs/src/components/ExampleComponent.vue
@@ -2,6 +2,8 @@
     <div>
       <h1>{{ message }}</h1>
       <h2>{{ 윤정서체크체크 }}</h2>
+      <h2>{{ getHello }}</h2>
+      <h2>{{ dbTest }}</h2>
     </div>
   </template>
   
@@ -12,7 +14,9 @@
     data() {
       return {
         message: '',
-        윤정서체크체크:'윤정서 체크체크'
+        윤정서체크체크:'윤정서 체크체크',
+        dbTest : '',
+        getHello:''
       };
     },
     mounted() {
@@ -23,6 +27,20 @@
         .catch(error => {
           console.error('There was an error!', error);
         });
-    },
+        axios.get('http://localhost/api')
+        .then(response => {
+          this.getHello = response.data;  // 응답 데이터 설정
+        })
+        .catch(error => {
+          console.error('There was an error!', error);
+        });
+      axios.get('http://localhost/api/visit')
+        .then(response => {
+          this.dbTest = response.data;  // 응답 데이터 설정
+        })
+        .catch(error => {
+          console.error('There was an error!', error);
+        });
+      },
   };
   </script>
