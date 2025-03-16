abea3e855e172c33c117065b1f2a2df4996eacc5
wasabi9812 <wasabi9812@naver.com>
2025-03-16 23:14:28 +0900

    Feat: 네이버 지도 기본 추가

--- frontend/vuejs/src/App.vue
+++ frontend/vuejs/src/App.vue
@@ -1,18 +1,20 @@
 <template>
   <img alt="Vue logo" src="./assets/logo.png">
   <ExampleComponent msg="Welcome to Your Vue.js App"/>
-
+  <NaverMap />
 </template>
 
 <script>
 import ExampleComponent from './components/ExampleComponent.vue';
+import NaverMap from './components/NaverMap.vue';
 
 
 
 export default {
   name: 'App',
   components: {
-    ExampleComponent
+    ExampleComponent,
+    NaverMap
   }
 }
 </script>
--- frontend/vuejs/vue.config.js
+++ frontend/vuejs/vue.config.js
@@ -17,5 +17,19 @@ module.exports = {
       }
     },
     allowedHosts: 'all'  // 모든 호스트에서의 접근 허용
-  }
-}
+  },
+
+
+    // Webpack 설정 커스터마이징 네이버 지도용 env활용 기본으로 vue에 있어서 도커 수정불필요
+    chainWebpack: (config) => {
+      config.plugin('html').tap((args) => {
+        // HtmlWebpackPlugin에 templateParameters 지정
+        args[0].templateParameters = {
+          BASE_URL: './',
+          VUE_APP_NAVER_CLIENT_ID: process.env.VUE_APP_NAVER_CLIENT_ID,
+          // 필요한 다른 환경 변수도 추가 가능
+        };
+        return args;
+      });
+    },
+};
