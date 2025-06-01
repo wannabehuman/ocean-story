3087e360df337296932fa54a31c4bc14fd97021e
이원기 <iwonki@iwongiui-MacBookAir.local>
2025-06-01 19:12:22 +0900

    커밋테스2트

--- handlers/commit-to-notion.js
+++ handlers/commit-to-notion.js
@@ -1,5 +1,7 @@
 import { registerModelHandler } from "@modelcontextprotocol/server-core";
 import { Client } from "@notionhq/client";
+import dotenv from "dotenv";
+dotenv.config();
 
 const notion = new Client({ auth: process.env.NOTION_TOKEN });
 const databaseId = process.env.NOTION_DATABASE_ID;
